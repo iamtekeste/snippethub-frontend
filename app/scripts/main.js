c59decd24cf0c95ab53f2bd917477426b1b6b3b1
Tekeste G <iamtekeste@gmail.com>
2016-05-17 15:58:02 -0500

    make select2 width 100%

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
  var $tags = $('#tag-list').select2({
-   width: "100%",
+   width: '100%',
    placeholder: 'Select tags'
  });
  $('#save-tag').on('click', function(e){
@@ -9,18 +9,18 @@
    var existing = [];
    if($tags.val() != null)
       existing = $tags.val();
-   var newtag = $("#tag-name").val();
-   var $option = $("<option></option");
+   var newtag = $('#tag-name').val();
+   var $option = $('<option></option');
    $option.val(newtag);
    $option.text(newtag);
-   $tags.append($option).trigger("change"); // add it to the list of selections
+   $tags.append($option).trigger('change'); // add it to the list of selections
 
    //after adding the option let's make it one of the selected
    //check if there was no selected tags first
    existing.push(newtag);
-   $tags.val(existing).trigger("change");
+   $tags.val(existing).trigger('change');
    //close modal after saving
-   $("#addTag").modal('hide');
-   $("#tag-name").val("");
+   $('#addTag').modal('hide');
+   $('#tag-name').val('');
  });
 //# sourceMappingURL=main.js.map
